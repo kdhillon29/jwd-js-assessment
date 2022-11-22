3f3046d3bc36225b2daf89272575b6682b76de68
kanwar Dhillon <kdhillon469@gmail.com>
2022-11-22 12:09:26 +1100

    assesement finished

--- js/index.js
+++ js/index.js
@@ -52,7 +52,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let quizDisplay = '';
     quizArray.map((quizItem, index) => {
       quizDisplay += `<ul class="list-group">
-                   Q - ${quizItem.q}
+                   Q${index+1} - ${quizItem.q}
                     <li class="list-group-item mt-2" id="li_${index}_0"><input type="radio" name="radio${index}" id="radio_${index}_0"> ${quizItem.o[0]}</li>
                     <li class="list-group-item" id="li_${index}_1"><input type="radio" name="radio${index}" id="radio_${index}_1"> ${quizItem.o[1]}</li>
                     <li class="list-group-item"  id="li_${index}_2"><input type="radio" name="radio${index}" id="radio_${index}_2"> ${quizItem.o[2]}</li>
@@ -66,6 +66,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // Calculate the score
   const calculateScore = () => {
     let score = 0;
+    let total = quizArray.length
     quizArray.map((quizItem, index) => {
       for (let i = 0; i < 4; i++) {
         //highlight the li if it is the correct answer
@@ -76,15 +77,104 @@ window.addEventListener('DOMContentLoaded', () => {
 
         if (quizItem.a == i) {
           //change background color of li element here
+          liElement.style.backgroundColor = 'green'
         }
 
         if (radioElement.checked) {
           // code for task 1 goes here
+          if (quizItem.a === i) {
+            score += 1;
+          }
+        }
+        if(radioElement.checked&&quizItem.a !== i){
+          liElement.style.borderColor = 'red'
         }
       }
+      console.log(`score is ${score}`)
+      document.querySelector('#score').innerHTML = `Score :<strong>${score}/${total}</strong> correct`
+      document.querySelector('#time').innerHTML=`Quiz Over.Your score :${score}`
+
     });
   };
 
   // call the displayQuiz function
   displayQuiz();
-});
+
+  // display score
+  let submitBtn = document.querySelector('#btnSubmit')
+  submitBtn.addEventListener('click', calculateScore)
+
+
+  //add question function
+
+  function addQuestion(question) {
+    // console.log(Object.keys(question).length)
+    // console.log(Object.values(question).length)
+
+    if (!(typeof question === 'object' && Object.keys(question).length === 3 && Object.values(question).length === 3)) {
+      console.log('question is not in correct format')
+    }
+    else {
+      quizArray.push(question);
+      displayQuiz();
+    }
+  }
+  // question objects
+  let question1 = {
+    q: 'Which is the largest planet in solar system?',
+    o: ['Saturn', 'Earth', 'Jupiter', 'Mars'],
+    a: 2
+
+  }
+  let question2 = {
+    q: 'Which is the largest State In Australia?',
+    o: ['VIC', 'WA', 'NSW', 'SA'],
+    a: 1
+  }
+
+  //call addQuestion function
+  addQuestion(question1)
+  addQuestion(question2)
+
+
+  //reload window
+  function resetQuiz() {
+    window.location.reload()
+  }
+  document.querySelector('#btnReset').addEventListener('click', resetQuiz)
+
+  ///   countdown timer
+
+  let quizTime = 1   //in mins
+  let newDate = new Date(new Date().getTime() + quizTime * 60000).getTime()  // in miliseconds
+
+  function countDown() {
+    let now = new Date().getTime()
+    let counter = newDate - now
+    let minutes = Math.floor((counter % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((counter % (1000 * 60)) / 1000);
+    //  let time =`${date.getMinutes()}: ${date.getSeconds()}`
+    console.log(minutes)
+    console.log(`seconds are ${seconds}`)
+    console.log(counter)
+
+    document.querySelector('#time').innerHTML = ` ${minutes} mins:${seconds} seconds `
+    if (counter <= 0) {
+      document.querySelector('#time').innerHTML = `Time up `
+      calculateScore();
+      submitBtn.disabled = true
+
+    }
+  }
+  const timer= setInterval(countDown, 1000);
+
+
+  //reset quiz timer on submit
+  function resetCountDown(){
+    clearInterval(timer)
+    document.querySelector('#time').innerHTML="Quiz Over"
+
+  }
+  submitBtn.addEventListener('click', resetCountDown)
+
+})
